21918ca307830f8357e20e4a64359af62af53b84
daniel-wales <165902783+daniel-wales@users.noreply.github.com>
2026-02-12 08:55:40 +0000

    Update and rename js/app.js to js/config/variables.js

---
+++ js/config/variables.js
@@ -0,0 +1,17 @@
+export const variableConfig = {
+  "ii Rate": [{ key: "ii Rate", label: "Interest Rate" }],
+  "CPI Level": [{ key: "CPI Level", label: "CPI Level" }],
+  "CPI Change": [
+    { key: "CPI QoQ", label: "CPI QoQ" },
+    { key: "CPI YoY", label: "CPI YoY" }
+  ],
+  "GDP Change": [
+    { key: "GDP QoQ", label: "GDP QoQ" },
+    { key: "GDP YoY", label: "GDP YoY" }
+  ]
+};
+
+export const colors = [
+  "#1f77b4", "#d62728", "#2ca02c",
+  "#ff7f0e", "#9467bd", "#17becf"
+];
