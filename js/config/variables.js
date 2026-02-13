ee399c105e463c03b05b3a625d09ac8fe42b41b3
daniel-wales <165902783+daniel-wales@users.noreply.github.com>
2026-02-13 08:18:35 +0000

    Update variables.js

--- js/config/variables.js
+++ js/config/variables.js
@@ -1,11 +1,52 @@
 export const variableConfig = {
-  "ii Rate": [{ key: "ii Rate", label: "Nominal Interest Rate" }],
-  "CPI QoQ Ann": [{ key: "CPI QoQ Ann", label: "Inflation (QoQ Ann)" }],
-  "CPI YoY": [{ key: "CPI YoY", label: "Inflation (YoY)" }],
-  "CPI Level": [{ key: "CPI Level", label: "Price Level" }],
-  "GDP QoQ Ann": [{ key: "GDP QoQ Ann", label: "GDP Growth (QoQ Ann)" }],
-  "GDP YoY": [{ key: "GDP YoY", label: "GDP Growth (YoY)" }],
-  "Real GDP Level": [{ key: "Real GDP Level", label: "Real GDP Level" }]
+  "ii Rate": [{
+    key: "ii Rate",
+    label: "Nominal Interest Rate",
+    unit: "percent",
+    yAxisLabel: "Percent"
+  }],
+
+  "CPI QoQ Ann": [{
+    key: "CPI QoQ Ann",
+    label: "Inflation (QoQ Ann)",
+    unit: "percent",
+    yAxisLabel: "Percent"
+  }],
+
+  "CPI YoY": [{
+    key: "CPI YoY",
+    label: "Inflation (YoY)",
+    unit: "percent",
+    yAxisLabel: "Percent"
+  }],
+
+  "CPI Level": [{
+    key: "CPI Level",
+    label: "Price Level",
+    unit: "index",
+    yAxisLabel: "Index"
+  }],
+
+  "GDP QoQ Ann": [{
+    key: "GDP QoQ Ann",
+    label: "GDP Growth (QoQ Ann)",
+    unit: "percent",
+    yAxisLabel: "Percent"
+  }],
+
+  "GDP YoY": [{
+    key: "GDP YoY",
+    label: "GDP Growth (YoY)",
+    unit: "percent",
+    yAxisLabel: "Percent"
+  }],
+
+  "Real GDP Level": [{
+    key: "Real GDP Level",
+    label: "Real GDP Level",
+    unit: "index",
+    yAxisLabel: "Index"
+  }]
 };
 
 export const colors = [
